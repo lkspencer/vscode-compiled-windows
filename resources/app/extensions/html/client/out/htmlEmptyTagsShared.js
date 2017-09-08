8e53b804f0c821c03de3d052a89ac9343a9a6df5
Kirk Spencer <kirk@lkspencer.com>
2017-09-08 04:39:51 -0700

    added version v1.16.0

---
+++ resources/app/extensions/html/client/out/htmlEmptyTagsShared.js
@@ -0,0 +1,8 @@
+"use strict";
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EMPTY_ELEMENTS = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/aa42e6ef8184e8ab20ddaa5682b861bfb6f0b2ad/extensions\html\client\out/htmlEmptyTagsShared.js.map
---
+++ resources/app/extensions/merge-conflict/out/interfaces.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CommitType;
+(function (CommitType) {
+    CommitType[CommitType["Current"] = 0] = "Current";
+    CommitType[CommitType["Incoming"] = 1] = "Incoming";
+    CommitType[CommitType["Both"] = 2] = "Both";
+})(CommitType = exports.CommitType || (exports.CommitType = {}));
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/aa42e6ef8184e8ab20ddaa5682b861bfb6f0b2ad/extensions\merge-conflict\out/interfaces.js.map
